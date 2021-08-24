224208c000d1d4527004d182e48fc3ed96aa2197
lucksslima <llima25.96@gmail.com>
2021-08-24 00:28:19 -0300

    correção: extensão do arquivo .py

--- infrastructure/s3_files.tf
+++ infrastructure/s3_files.tf
@@ -3,5 +3,5 @@ resource "aws_s3_bucket_object" "spark_job" {
     key = "emr-code/pyspark/pyspark_teste_csv_from_tf.py"
     acl = "private"
     source = "../pyspark_teste_csv.py"
-    etag = filemd5("../pyspark_teste_csv")
+    etag = filemd5("../pyspark_teste_csv.py")
 }
